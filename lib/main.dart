8ce51147bf3ca4fcd8619e311d2ed3c15aa1b25a
Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>
2022-12-17 20:35:30 +0530

    style changes
    Signed-off-by: Anuj Singh Rawat <84454648+anujsinghrawat@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -5,43 +5,57 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
-
+class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()=> _MyApp();
-  }
+  State<StatefulWidget> createState() => _MyApp();
+}
 
-
-class _MyApp extends State<MyApp>{
-  final List<Widget> _list=[
-    Text("Event1")
+class _MyApp extends State<MyApp> {
+  final List<Widget> _list = [
+    Text("Event1"),
   ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Event Scheduler"),
+          // foregroundColor: Color.fromARGB(2, 2, 0, 61),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 70, 124, 129),
+          title: const Text(" ⚡Event Scheduler⚡ "),
         ),
-        body: ListView.builder(
-            itemCount: _list.length,
-            itemBuilder: ((contex,index)=>_list[index])),
-        floatingActionButton: Builder(
-          builder: (context) {
-            return FloatingActionButton(
-              onPressed: () async {
-                String newText = await Navigator.of(context).push(MaterialPageRoute(builder:(context)=> NewEventScreen()));
-                setState(() {
-                  _list.add(Text(newText));
-                });
-                },
-              child: Icon(Icons.add),
-            );
-          }
+        body: Container(
+          color: Color(0xffDAF5FF),
+          padding: EdgeInsets.all(50),
+          child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _list.length,
+              // itemBuilder: ((contex, index) => _list[index]),
+              itemBuilder: (BuildContext context, int index) {
+              return Container(
+                padding: const EdgeInsets.all(8),
+                height: 50,
+                color: Color.fromARGB(255, 253, 219, 202),
+                child: Center(child: _list[index]),
+    );
+  }
+              ),
         ),
+        floatingActionButton: Builder(builder: (context) {
+          return FloatingActionButton(
+            onPressed: () async {
+              String newText = await Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => NewEventScreen()));
+              setState(() {
+                _list.add(Text(newText));
+              });
+            },
+            backgroundColor: const Color.fromARGB(255, 160, 16, 185),
+            child: Icon(Icons.add),
+          );
+        }),
       ),
     );
   }
-
 }
-
--- lib/new_event.dart
+++ lib/new_event.dart
@@ -1,33 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NewEventScreen extends StatefulWidget{
+class NewEventScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _NewEventScreen();
 }
 
 class _NewEventScreen extends State<NewEventScreen> {
-  final TextEditingController _textEditingController= TextEditingController();
+  final TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("New Event"),
+        backgroundColor: const Color.fromARGB(255, 160, 16, 185),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _textEditingController,
-          ),
-          Builder(
-            builder: (context) {
-              return ElevatedButton(onPressed: () {
-                String newEventText=_textEditingController.text;
-                Navigator.of(context).pop(newEventText);
-              }, child: Text("Dabao"));
-            }
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.all(50),
+        child: Column(
+          
+          children: [
+            TextField(
+              
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              borderSide: BorderSide(color: Color.fromARGB(255, 177, 33, 243))),
+                prefixIcon: Icon(Icons.calendar_month_rounded),
+                border: OutlineInputBorder(),
+                labelText: 'Event',
+              ),
+            ),
+            Builder(builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  String newEventText = _textEditingController.text;
+                  Navigator.of(context).pop(newEventText);
+                },
+                child: Text("ADD"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(
+                        const Color.fromARGB(255, 160, 16, 185))),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
